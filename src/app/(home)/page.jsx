99a59ee484031c11a126789dd5381b866490a0df
Aryan srivastava <mainaryansrivastav@gmail.com>
2024-07-12 08:40:14 +0530

    update

--- src/app/(home)/page.jsx
+++ src/app/(home)/page.jsx
@@ -1,8 +1,9 @@
 'use client'
 
+import { useEffect } from 'react';
+import dynamic from 'next/dynamic';
 import Header from "./components/Header/Header";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min';
 import Hero from "./components/Hero/Hero";
 import CardSection from "./components/CardSection/CardSection";
 import AddOwn from "./components/AddOwn/AddOwn";
@@ -11,17 +12,27 @@ import GetStart from "./components/GetStart/GetStart";
 import Footer from "./components/Footer/Footer";
 import Copyright from "./components/Copyright/Copyright";
 
+// Dynamically import bootstrap JS
+const BootstrapJS = dynamic(
+  () => import('bootstrap/dist/js/bootstrap.bundle.min'),
+  { ssr: false }
+);
+
 export default function Home() {
+  useEffect(() => {
+    BootstrapJS();
+  }, []);
+
   return (
-   <>
-   <Header/>
-   <Hero/>
-   <CardSection/>
-   <AddOwn/>
-   <Testimonials/>
-   <GetStart/>
-   <Footer/>
-   <Copyright/>
-   </>
+    <>
+      <Header />
+      <Hero />
+      <CardSection />
+      <AddOwn />
+      <Testimonials />
+      <GetStart />
+      <Footer />
+      <Copyright />
+    </>
   );
 }
